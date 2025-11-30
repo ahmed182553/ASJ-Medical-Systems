8d25f4b91b06bc33cd795a54662dba11819fa2c4
ahmed182553 <iooyoussef3@gmail.com>
2025-11-30 02:44:57 +0200

    update cards design

---
+++ src/components/Section2/Card1section2.jsx
@@ -0,0 +1,31 @@
+import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
+import image5 from "./image5.jpg";
+export default function Card1section2() {
+  return (
+    <>
+      {" "}
+      <div className="w-[320px] h-[330px] bg-white rounded-3xl shadow-md relative overflow-hidden cursor-pointer transition-all duration-300 hover:scale-[1.03]">
+        <div className="w-full h-[70%] overflow-hidden">
+          <img
+            src={image5}
+            alt="X-Ray"
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        <div className="absolute bottom-[30%] left-0 w-full h-[120px] bg-linear-to-t from-white via-white/80 to-transparent"></div>
+        <div className="absolute bottom-[20%] left-4 flex flex-col justify-between">
+          <p className="text-[15px] font-semibold tracking-widest text-gray-500">
+            Systems
+          </p>
+          <h2 className="text-[25px] font-semibold text-gray-900 mt-2">
+            X-Ray
+          </h2>
+        </div>
+        <button className="absolute bottom-4 right-4 bg-white shadow-md w-12 h-12 rounded-full flex items-center justify-center hover:bg-blue-700 hover:scale-110 transition delay-200 cursor-pointer">
+          <ArrowUpRightIcon className="w-5 h-5 text-gray-700 hover:scale-110 transition " />
+        </button>
+      </div>
+    </>
+  );
+}
--- src/components/Section2/Div1section2.jsx
+++ src/components/Section2/Div1section2.jsx
@@ -1,15 +1,28 @@
+import Card1section2 from "./Card1section2";
+import Card2section2 from "./Card2section2";
+import Card3section2 from "./Card3section2";
+import Card4section2 from "./Card4section2";
 export default function Div1section2() {
   return (
     <>
-      <div className="h-full flex justify-center items-center bg-[#dff0f1] w-full p-3 flex-col">
-        <div className="  h-8 bg-white rounded-4xl w-30 flex justify-center items-center">
+      <div className="h-[90dvh] flex justify-between items-center bg-[#dff0f1] w-full p-3 flex-col">
+        <div className="h-8 bg-white rounded-4xl w-30 flex justify-center items-center">
           <h1 className="text-blue-700 font-medium text-[14px] leading-3.5 tracking-widest">
             OUR SERVICES
           </h1>
         </div>
-        <div className="w-[55%] p-3 text-6xl font-bold">
+        <div className="w-[55%] p-3 text-6xl font-semibold text-center">
           <h1>High Quality Test Services</h1>
-          <h1>jbfdksjkfhskgsfb</h1>
+        </div>
+
+        <div className="w-[95%] h-[60%] flex gap-4 rounded-4xl justify-center">
+          <Card1section2 />
+
+          <Card2section2 />
+
+          <Card3section2 />
+
+          <Card4section2 />
         </div>
       </div>
     </>
