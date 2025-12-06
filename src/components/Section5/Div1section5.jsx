5e4e2e57992f1937164316a4f5b1448802371a75
ahmed182553 <iooyoussef3@gmail.com>
2025-12-06 04:21:04 +0200

    update

---
+++ src/components/Section5/Div1section5.jsx
@@ -0,0 +1,64 @@
+import image11 from "./image11.png";
+import { motion } from "framer-motion";
+
+export default function DivSectionExample() {
+  return (
+    <div className="w-full flex justify-center py-10">
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7 }}
+        viewport={{ once: true }}
+        className="
+          w-[90%] h-[350px] bg-[#1f59c5] rounded-3xl relative overflow-hidden
+          flex flex-col items-center text-center
+          sm:h-[420px]
+          sm:py-8
+        "
+      >
+        <div
+          className="absolute inset-0 bg-cover bg-center opacity-20"
+          style={{ backgroundImage: `url(${image11})` }}
+        />
+        <div
+          className="
+            relative z-10 flex flex-col items-center justify-between h-full py-6
+            sm:gap-4 
+          "
+        >
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.9 }}
+            className="
+              bg-white text-blue-800 px-6 py-2 rounded-full font-semibold shadow
+              sm:px-5 sm:py-2 sm:text-sm
+            "
+          >
+            BOOK YOUR TEST
+          </motion.button>
+          <motion.h1
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.7 }}
+            viewport={{ once: true }}
+            className="
+              text-white text-3xl md:text-5xl font-bold leading-snug
+              sm:text-2xl sm:leading-tight 
+            "
+          >
+            You can Check more here About Available <br />
+            Services and Systems & Parts
+          </motion.h1>
+          <motion.button
+            whileHover={{ scale: 1.08 }}
+            whileTap={{ scale: 0.9 }}
+            className="sm:px-6 sm:py-2 sm:text-sm bg-white text-blue-800 px-8 py-3 rounded-full font-semibold shadow  hover:bg-blue-500 transition duration-300 hover:text-white cursor-pointer
+            "
+          >
+            CONTACT US
+          </motion.button>
+        </div>
+      </motion.div>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Herosection/Header.jsx";
 import Div1section2 from "./components/Section2/Div1section2.jsx";
 import Div1section3 from "./components/Section3/Div1section3.jsx";
 import Div1section4 from "./components/Section4/Div1section4.jsx";
+import Div1section5 from "./components/Section5/Div1section5.jsx";
 
 createRoot(document.getElementById("root")).render(
   <>
@@ -11,5 +12,6 @@ createRoot(document.getElementById("root")).render(
     <Div1section2 />
     <Div1section3 />
     <Div1section4 />
+    <Div1section5 />
   </>
 );
